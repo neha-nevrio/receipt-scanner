3d86dc0059df726f5cc774b6c79838a414ad15e9
prince-nevrio <prince@nevrio.tech>
2023-06-23 17:53:34 +0530

    feat:working-restapi

--- receipt_scanner.py
+++ receipt_scanner.py
@@ -3,12 +3,23 @@ from docquery import document, pipeline
 def receipt_scanner(image_path):
     p = pipeline('document-question-answering')
     doc = document.load_document(image_path)
-    for q in ["What is the invoice number?", "What is the invoice total?"]:
+    resultArr = []
+    for q in ["name","date","What is the invoice number?", "What is the invoice total?"]:
 
         result = (p(question=q, **doc.context))
-        print(result)
 
-    return result
+        resultArr.append(result[0]['answer'])
+
+    resultJson = {
+        "Seller name": resultArr[0],
+        "Date": resultArr[1],
+        "Invoice number": resultArr[2],
+        "Total amount": resultArr[3]
+    }
+
+
+
+    return resultJson
 
 
 
